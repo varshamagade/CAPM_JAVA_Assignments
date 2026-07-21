c24d3acd78a07d28870121265919baf271bad66a
RVarsha0902 <varsha.yuvaraj.magade@sap.com>
2026-07-21 14:10:17 +0530

    First Java assignments

---
+++ BankAccountSimulator.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class BankAccountSimulator {
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+        String accountHolder;
+        int accountNumber = 1001;
+        double balance;
+        int Choice;
+
+        System.out.println("Enter Account Holder Name");
+        accountHolder = sc.next();
+
+        System.out.println("Enter Account Number");
+        accountNumber = sc.nextInt();
+
+        System.out.print("Enter Initial Balance: ");        
+        balance = sc.nextDouble();
+        
+        do{
+            System.out.println("====Bank Menu====");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            Choice = sc.nextInt();
+
+            switch (Choice) {
+                case 1:
+                    //Deposit
+                    System.out.print("Enter amount you wish to deposit: ");
+                    double deposit = sc.nextDouble();
+                    balance += deposit;
+                    System.out.printf("Deposited: %.2f%n", deposit);
+                    System.out.printf("Current Balance: %.2f%n", balance);
+                    break;
+                case 2:
+                    //Deposit
+                    System.out.print("Enter amount you wish withdrawn: ");
+                    double Withdrawl = sc.nextDouble();
+
+                    if (Withdrawl < balance) {
+                    balance -= Withdrawl;
+                    System.out.printf("Withdrawl: %.2f%n", Withdrawl);
+                    System.out.printf("Current Balance: %.2f%n", balance);
+                    break;
+                    }
+                    else if (Withdrawl > balance) {
+                        System.out.printf("Insufficient balance!!");
+                        break;
+                    }
+                case 3:
+                    System.out.printf("Current Balance: %.2f%n", balance);
+                    break;
+                case 0:
+                      // Step 6: Exit
+                      System.out.println("\nThank you, " + accountHolder + "for banking with us");
+               
+                    break;
+            
+                default:
+                    System.out.print("Invalid Choice ");
+                    break;
+            }
+        }while (Choice != 0);
+      sc.close();
+    }
+    
+}
---
+++ helloworld.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class helloworld
+{
+   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Hello World");
+   
+    byte b = 120;
+    int  i= 1234;
+    char c = 'd';
+    long l = 1233456789975L;
+    double d = 23114222.444;
+    float f = 2.34f;
+    
+    System.out.println(b + ","+ i + "," + c + "," + l + "," + d  + "," + f);
+
+    System.out.println("Enter which day : ");
+    int day = sc.nextInt();
+
+    if(day > 7)
+    {
+          System.out.println("invalid input ");
+    }
+    else
+    {
+        switch (day) {
+            case 1:
+                
+               System.out.println("Monday "); 
+               break;
+            case 2:
+                
+               System.out.println("Tuesday "); 
+               break;
+            case 3:
+                
+               System.out.println("Wednesday "); 
+               break;
+            case 4:
+                
+               System.out.println("Thursday "); 
+               break;
+             case 5:
+                
+               System.out.println("friday "); 
+
+            case 6:
+                
+               System.out.println("saturday "); 
+               break;
+             case 7:
+                
+               System.out.println("sunday "); 
+               break;
+        
+            default:
+                break;
+        }
+    }
+
+    sc.close();
+    } 
+}
